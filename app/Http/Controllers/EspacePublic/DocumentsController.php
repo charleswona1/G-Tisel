3ef8c3d2cefcd77599db5f91e01771f0cedd00b8
charleswona1 <muriel.wona@abyster.com>
2021-10-18 10:22:05 +0200

    documentation espace public

---
+++ app/Http/Controllers/EspacePublic/DocumentsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\EspacePublic;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class DocumentsController extends Controller
+{
+    public function juridic(){
+        return view("documents.textJuridique");
+    }
+
+    public function regime(){
+        return view("documents.regime");
+    }
+
+    public function procedure(){
+        return view("documents.procedure");
+    }
+
+    public function download(Request $request)
+    {
+        return Storage::download('/public/textes/'.$request->fichier);
+    }
+}
