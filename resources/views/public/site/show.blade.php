39960c6b0c2907fa5bf712cc9afe8cb273d28c5f
TRESORJOSE <tresor.nguidjol@gmail.com>
2021-12-06 10:46:54 +0100

    commit cousin

--- resources/views/public/site/show.blade.php
+++ resources/views/public/site/show.blade.php
@@ -8,9 +8,9 @@
                         <div>
                             <a class="btn btn-primary btn-sm" href="{{route('public.demand',$site)}}" role="button">Demander</a>
                         </div>
-                        
+
                     </div>
-                    
+
                 </div>
                 @php
                     \Debugbar::info($site->UploadFileSite)
@@ -19,7 +19,7 @@
 
                     <div class="row mx-auto my-auto justify-content-center">
                         <div id="recipeCarousel" class="carousel slide" data-bs-ride="carousel">
-                            
+
                             <div class="carousel-inner"  role="listbox">
                                 @forelse ($site->UploadFileSite as $key=>$patchImg)
                                     <div class="carousel-item {{$key == 0? "active":""}}">
@@ -37,9 +37,9 @@
                                         </div>
                                     </div>
                                 @empty
-                                    
+
                                 @endforelse
-                            
+
                             </div>
                             <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev">
                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -53,35 +53,37 @@
                     <div class="d-flex px-3 mt-4">
                         <div class="icon-grey-ligth" style="margin-right: 10px;"><i class="far fa-file-contract"></i></div>
                         <p class="subtitle-calification-site" > {{$site->Regime->name}} </p>
-                    
+
                     </div>
 
-                    <div class="d-flex px-3 pt-2">
-                        <div class="icon-grey-ligth" style="margin-right: 10px;"><i class="far fa-briefcase"></i></div>
-                        <p class="subtitle-calification-site" > 
-                           <!-- @forelse ($site->Regime->Activites as $activite)
-                                {{$activite->libelle_activite}}
+                    <div class="px-3 pt-2">
+                        <div class="d-flex">
+                            <div class="icon-grey-ligth" style="margin-right: 10px;"><i class="far fa-briefcase"></i></div>
+                            <p class="subtitle-calification-site" > Activités proposées</p>
+                        </div>
+
+                        <ul class="list-group list-group-flush ps-3">
+                            @forelse ($site->Regime->Activites as $activite)
+                                <li class="list-group-item">{{$activite->libelle_activite}}</li>
                             @empty
-                                Aucune activité
-                            @endforelse !-->
-                            {{$site->Regime->Activites[0]->libelle_activite}}
-                        </p>
-                    
+                                <li class="list-group-item">Aucune activité disponible</li>
+                            @endforelse
+                        </ul>
                     </div>
 
                     <div class="d-flex px-3 pt-2">
                         <div class="icon-grey-ligth" style="margin-right: 10px;"><i class="far fa-map-marker"></i></div>
-                        
+
                         <p class="subtitle-localite-site" > {{$site->Arrondissement->name}}, {{$site->localite}} </p>
                     </div>
 
                     <div class="d-flex px-3 pt-2">
                         <div class="icon-grey-ligth" style="margin-right: 10px;"><i class="far fa-bolt"></i></div>
-                        
+
                         <p class="subtitle-capacite-site" >potentiel de production:  {{$site->capacite}} Mwatt</p>
                     </div>
 
-                    
+
 
                     <div class="px-3 mt-4">
                         <h6 class="fw-bold mb-2">Descriptions</h6>
@@ -90,24 +92,24 @@
                             {{$site->description}}
                         </p>
                     </div>
-                    
+
                     <hr>
                     <div class="d-flex justify-content-between px-3 ">
-                        <div class="d-flex" > 
+                        <div class="d-flex" >
                             <div class="icon-grey-ligth" style="margin-right: 5px;"><i class="far fa-calendar-alt"></i></div>
-                           <p class="subtitle-detail-site mb-0">{{date('d/m/Y', strtotime($site->created_at))}} </p> 
+                           <p class="subtitle-detail-site mb-0">{{date('d/m/Y', strtotime($site->created_at))}} </p>
                         </div>
 
-                        <div class="subtitle-detail-site d-flex" > 
+                        <div class="subtitle-detail-site d-flex" >
                             <div class="icon-grey-ligth" style="margin-right: 5px;"><i class="far fa-users"></i></div>
                             <p class="subtitle-detail-site mb-0"> 6 {{__('site postulant')}}</p>
                         </div>
 
-                        <div class="subtitle-detail-site d-flex" > 
+                        <div class="subtitle-detail-site d-flex" >
                             <div class="statusIndex rounded-circle" style="background-color: #297dc8;"></div>
-                            <p class="subtitle-detail-site mb-0">En cours</p> 
+                            <p class="subtitle-detail-site mb-0">En cours</p>
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
@@ -124,7 +126,7 @@
                         <li class="list-group-item">
 
                             <div class="row">
-                                
+
                                 <div class="col-3 justify-content-center">
                                     <span class="border border-light">
                                         <img src="{{asset('assets/img/chutte.jpg')}}" style="height: 40px;" alt="item1" class="rounded">
@@ -133,38 +135,38 @@
 
                                 <div class="col-7">
                                     <p class="title-site" >{{$demandeTitre->site->name}}</p>
-                                
+
                                     <div class="d-flex">
                                         <div class="icon-grey-ligth" style="margin-right: 10px;"><i class="far fa-briefcase"></i></div>
                                         <p class="subtitle-calification-site" > {{$demandeTitre->site->Regime->Activites[0]->libelle_activite}}</p>
-                                    
+
                                     </div>
                                     <div class="d-flex">
                                         <div class="icon-grey-ligth" style="margin-right: 10px;"><i class="far fa-map-marker"></i></div>
-                                        
+
                                         <p class="subtitle-localite-site" > {{$demandeTitre->site->Arrondissement->name}}, {{$demandeTitre->site->localite}} </p>
                                     </div>
                                 </div>
 
                                 <div class="col-2 d-flex justify-content-end icon-grey-dark">
                                     <i class="fas fa-ellipsis-h mb-lg-1 me-3 me-lg-0"></i>
-                                </div>    
-                                
+                                </div>
+
 
                                 <div class="d-flex justify-content-between mt-1">
-                                    <div class="d-flex" > 
+                                    <div class="d-flex" >
                                         <div class="icon-grey-ligth" style="margin-right: 5px;"><i class="far fa-file-signature"></i></div>
                                         <div>
-                                            <p class="subtitle-labe-service-site mb-0">{{__('site service')}}</p> 
-                                            <p class="subtitle-detail-site mb-0">{{ '---' }}</p> 
+                                            <p class="subtitle-labe-service-site mb-0">{{__('site service')}}</p>
+                                            <p class="subtitle-detail-site mb-0">{{ '---' }}</p>
                                         </div>
                                     </div>
 
-                                    <div class="subtitle-detail-site d-flex" > 
+                                    <div class="subtitle-detail-site d-flex" >
                                         <div class="statusIndex rounded-circle" style="background-color: {{$demandeTitre->status == 'Pending' ? '#37c40f' : ( $demandeTitre->status == 'Processed' ? '#89b2c1' : ( $demandeTitre->status == 'success' ? '#297dc8' : '#f40b0b'))}} ;"></div>
-                                        <p class="subtitle-detail-site mb-0"> {{$demandeTitre->status == 'Pending' ? 'En Attente' : ( $demandeTitre->status == 'Processed' ? 'En Cours' : ( $demandeTitre->status == 'success' ? 'Attribué' : 'Rejeté'))}}</p> 
+                                        <p class="subtitle-detail-site mb-0"> {{$demandeTitre->status == 'Pending' ? 'En Attente' : ( $demandeTitre->status == 'Processed' ? 'En Cours' : ( $demandeTitre->status == 'success' ? 'Attribué' : 'Rejeté'))}}</p>
                                     </div>
-                                    
+
                                 </div>
 
                         </li>
@@ -195,20 +197,20 @@
 
         /* medium and up screens */
         @media (min-width: 768px) {
-            
+
             .carousel-inner .carousel-item-end.active,
             .carousel-inner .carousel-item-next {
             transform: translateX(25%);
             }
-            
-            .carousel-inner .carousel-item-start.active, 
+
+            .carousel-inner .carousel-item-start.active,
             .carousel-inner .carousel-item-prev {
             transform: translateX(-25%);
             }
         }
 
         .carousel-inner .carousel-item-end,
-        .carousel-inner .carousel-item-start { 
+        .carousel-inner .carousel-item-start {
         transform: translateX(0);
         }
     </style>
