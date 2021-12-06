51c0effd9ddab8c74d874176c9ccfbff0bd9fb1d
TRESORJOSE <tresor.nguidjol@gmail.com>
2021-12-06 11:48:18 +0100

    my comitt

--- resources/views/home/site.blade.php
+++ resources/views/home/site.blade.php
@@ -9,8 +9,8 @@
                 <div class="card-body">
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item title-item-filtre pointer" id="reset_filter">{{__('site all')}}</li>
-                        <li class="list-group-item title-item-filtre pointer" 
-                            data-bs-toggle="modal" 
+                        <li class="list-group-item title-item-filtre pointer"
+                            data-bs-toggle="modal"
                             data-bs-target="#regime">
                                 {{__('site regime')}}
                                 @if ($optRegime)
@@ -19,8 +19,8 @@
                                     <small style="font-size: 10px">{{$optRegime->name}}</small>
                                 @endif
                         </li>
-                        <li class="list-group-item title-item-filtre pointer" 
-                            data-bs-toggle="modal" 
+                        <li class="list-group-item title-item-filtre pointer"
+                            data-bs-toggle="modal"
                             data-bs-target="#Source">
                                 {{__('site source energie')}}
                                 @if ($optSource)
@@ -29,8 +29,8 @@
                                     <small style="font-size: 10px">{{$optSource->name}}</small>
                                 @endif
                         </li>
-                        <li class="list-group-item title-item-filtre pointer" 
-                            data-bs-toggle="modal" 
+                        <li class="list-group-item title-item-filtre pointer"
+                            data-bs-toggle="modal"
                             data-bs-target="#localisation">
                                 {{__('site zone')}}
                                 @if ($optArrond)
@@ -49,75 +49,75 @@
                 <div class="card-header pt-4 pb-0">
                     <h1 class="title mb-0" >{{__('site title')}}</h1>
                     <p class="subtitle mb-1">{{__('site subtitle')}}</p>
-                    
+
                 </div>
                 <div class="card-body pt-0">
                     <ul class="list-group list-group-flush">
-                        
+
                             @forelse ($sites as $site)
                             <li class="list-group-item mt-2">
                                 <div class="row item-site">
                                     <div class="col-2 justify-content-center">
                                         <span class="border border-light">
                                         
-                                            <img src="{{asset("storage/".$sites[0]->UploadFileSite[0]->url)}}" style="height: 40px;" alt="item1" class="rounded">    
+                                            <img src="{{asset("storage/".$sites[0]->UploadFileSite[0]->url)}}" style="height: 40px;" alt="item1" class="rounded">
                                         </span>
                                     </div>
 
                                     <div class="col-8">
-                                        <a href="{{Route('show',$site)}}"> 
+                                        <a href="{{Route('show',$site)}}">
                                             <p class="title-site"  >{{$site->name}}</p>
                                         </a>
-                                        
+
                                         <p class="mb-1 description-site"> {{$site->description}} </p>
                                         <div class="d-flex">
                                             <div class="icon-grey-ligth" style="margin-right: 10px;"><i class="far fa-briefcase"></i></div>
-                                            <p class="subtitle-calification-site" > 
+                                            <p class="subtitle-calification-site" >
                                                <!--  @forelse ($site->Regime->Activites as $activite)
                                                     {{$activite->libelle_activite}}
                                                 @empty
                                                     Aucune activité
                                                 @endforelse -->
                                                 {{-- {{$site->Regime->Activites[0]->libelle_activite}} --}}
-                                                
+
                                             </p>
-                                        
+
                                         </div>
                                         <div class="d-flex">
                                             <div class="icon-grey-ligth" style="margin-right: 10px;"><i class="far fa-map-marker"></i></div>
-                                            
+
                                             <p class="subtitle-localite-site" > {{$site->Arrondissement->name}}, {{$site->localite}} </p>
                                         </div>
-                                    
+
                                         <div class="d-flex">
                                             <div class="icon-grey-ligth" style="margin-right: 10px;"><i class="far fa-bolt"></i></div>
-                                            
+
                                             <p class="subtitle-capacite-site" > {{$site->capacite}} Mwatt</p>
                                         </div>
-                                        
+
 
                                         <div class="d-flex justify-content-between">
-                                            <div class="d-flex" > 
+                                            <div class="d-flex" >
                                                 <div class="icon-grey-ligth" style="margin-right: 5px;"><i class="far fa-calendar-alt"></i></div>
-                                            <p class="subtitle-detail-site mb-0 mt-1">{{date('d/m/Y', strtotime($site->created_at))}} </p> 
+                                            <p class="subtitle-detail-site mb-0 mt-1">{{date('d/m/Y', strtotime($site->created_at))}} </p>
                                             </div>
 
-                                            <div class=" d-flex " > 
+                                            <div class=" d-flex " >
                                                 <div class="icon-grey-ligth" style="margin-right: 5px;"><i class="far fa-users"></i></div>
                                                 <p class="subtitle-detail-site mb-0 mt-1"> 6 {{__('site postulant')}}</p>
                                             </div>
 
-                                            <div class="d-flex mt-1" > 
+                                            <div class="d-flex mt-1" >
                                                 <div class="statusIndex rounded-circle" style="background-color: #297dc8;"></div>
-                                                <p class="subtitle-detail-site mb-0 ">En cours</p> 
+                                                <p class="subtitle-detail-site mb-0 ">En cours</p>
                                             </div>
-                                            
+
                                         </div>
                                     </div>
 
                                     <div class="col-2 d-flex justify-content-end icon-grey-dark">
                                         <i class="fas fa-ellipsis-h mb-lg-1 me-3 me-lg-0"></i>
-                                    </div>        
+                                    </div>
                                 </div>
                             </li>
                             @empty
@@ -125,11 +125,11 @@
                                 <h4>Aucun site disponible</h4>
                             </li>
                             @endforelse
-                            
-                            
+
+
                         </li>
 
-                        
+
                     </ul>
                 </div>
             </div>
@@ -145,7 +145,7 @@
                         <li class="list-group-item">
 
                             <div class="row">
-                                
+
                                 <div class="col-3 justify-content-center">
                                     <span class="border border-light">
                                         <img src="{{ asset('assets/img/chutte.jpg')}}" style="height: 40px;" alt="item1" class="rounded">
@@ -154,45 +154,45 @@
 
                                 <div class="col-7">
                                     <p class="title-site" >{{$demandeTitre->site->name}}</p>
-                                
+
                                     <div class="d-flex">
                                         <div class="icon-grey-ligth" style="margin-right: 10px;"><i class="far fa-briefcase"></i></div>
                                         <p class="subtitle-calification-site" > {{$demandeTitre->site->Regime->Activites[0]->libelle_activite}}</p>
-                                    
+
                                     </div>
                                     <div class="d-flex">
                                         <div class="icon-grey-ligth" style="margin-right: 10px;"><i class="far fa-map-marker"></i></div>
-                                        
+
                                         <p class="subtitle-localite-site" > {{$demandeTitre->site->Arrondissement->name}}, {{$demandeTitre->site->localite}} </p>
                                     </div>
                                 </div>
 
                                 <div class="col-2 d-flex justify-content-end icon-grey-dark">
                                     <i class="fas fa-ellipsis-h mb-lg-1 me-3 me-lg-0"></i>
-                                </div>    
-                                
+                                </div>
+
 
                                 <div class="d-flex justify-content-between mt-1">
-                                    <div class="d-flex" > 
+                                    <div class="d-flex" >
                                         <div class="icon-grey-ligth" style="margin-right: 5px;"><i class="far fa-file-signature"></i></div>
                                         <div>
-                                            <p class="subtitle-labe-service-site mb-0">{{__('site service')}}</p> 
-                                            <p class="subtitle-detail-site mb-0">{{'---' }}</p> 
+                                            <p class="subtitle-labe-service-site mb-0">{{__('site service')}}</p>
+                                            <p class="subtitle-detail-site mb-0">{{'---' }}</p>
                                         </div>
                                     </div>
 
-                                    <div class="subtitle-detail-site d-flex" > 
+                                    <div class="subtitle-detail-site d-flex" >
                                         <div class="statusIndex rounded-circle" style="background-color: {{$demandeTitre->status == 'Pending' ? '#37c40f' : ( $demandeTitre->status == 'Processed' ? '#89b2c1' : ( $demandeTitre->status == 'success' ? '#297dc8' : '#f40b0b'))}} ;"></div>
-                                        <p class="subtitle-detail-site mb-0"> {{$demandeTitre->status == 'Pending' ? 'En Attente' : ( $demandeTitre->status == 'Processed' ? 'En Cours' : ( $demandeTitre->status == 'success' ? 'Attribué' : 'Rejeté'))}}</p> 
+                                        <p class="subtitle-detail-site mb-0"> {{$demandeTitre->status == 'Pending' ? 'En Attente' : ( $demandeTitre->status == 'Processed' ? 'En Cours' : ( $demandeTitre->status == 'success' ? 'Attribué' : 'Rejeté'))}}</p>
                                     </div>
-                                    
+
                                 </div>
 
                         </li>
                         @empty
                             <h5>Vous n'avez aucune demande de site</h5>
                         @endforelse
-                
+
                     </ul>
                 </div>
             </div>
@@ -207,7 +207,7 @@
                     <h6>Filtre sur les regimes</h6>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                
+
                 <div class="p-2">
                     @forelse ($regimes as $regime)
                         <div class="form-check">
@@ -225,7 +225,7 @@
                     <button type="button" class="btn btn-sm btn-danger" data-bs-dismiss="modal" aria-label="Close">
                         <i class="fas fa-times"></i>
                     </button>
-                    
+
                     <button type="button" class="btn btn-sm btn-primary" data-bs-dismiss="modal" id="optregime" aria-label="find">
                         <i class="fas fa-check"></i>
                     </button>
@@ -261,7 +261,7 @@
                     <button type="button" class="btn btn-sm btn-danger" data-bs-dismiss="modal" aria-label="Close">
                         <i class="fas fa-times"></i>
                     </button>
-                    
+
                     <button type="button" class="btn btn-sm btn-primary" id="optsource" data-bs-dismiss="modal" aria-label="find">
                         <i class="fas fa-check"></i>
                     </button>
@@ -280,7 +280,7 @@
                     <h6>Filtre sur la localistion</h6>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                
+
                 <div class="p-2">
                     <div class="row">
                         <div class="col-lg-4">
@@ -316,14 +316,14 @@
                             </x-form-group>
                         </div>
                     </div>
-                   
+
                 </div>
 
                 <div class="d-flex justify-content-between">
                     <button type="button" class="btn btn-sm btn-danger" data-bs-dismiss="modal" aria-label="Close">
                         <i class="fas fa-times"></i>
                     </button>
-                    
+
                     <button type="button" class="btn btn-sm btn-primary" id="optarrond" data-bs-dismiss="modal" aria-label="find">
                         <i class="fas fa-check"></i>
                     </button>
@@ -367,7 +367,7 @@
                 $('#region').on('change',function(e){
                     $('#depart').empty();
                     let select ="";
-            
+
                     $.ajax({
                         url: "{{route('search')}}",
                         type: "GET",
@@ -383,17 +383,17 @@
                             $('#depart').append(select);
                         },
                         error: function(response){
-                        
+
                         }
                     });
-                   
+
                 });
 
                 $('#depart').on('change',function(e){
-                    
+
                     $('#arrond').empty();
                     let select ="";
-            
+
                     $.ajax({
                         url: "{{route('search')}}",
                         type: "GET",
@@ -409,7 +409,7 @@
                             $('#arrond').append(select);
                         },
                         error: function(response){
-                        
+
                         }
                     });
                 });
@@ -424,7 +424,7 @@
                             return response;
                         },
                         error: function(response){
-                        
+
                         }
                     });
                 }
@@ -440,7 +440,7 @@
                 $('#optarrond').on('click',function () {
                     filterArrondissement('{{route("sites")}}',$('#arrond').val())
                 });
-                
+
                 $('#reset_filter').on('click',function () {
                     window.location = '{{route("sites")}}';
                 })
